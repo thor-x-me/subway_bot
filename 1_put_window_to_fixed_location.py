6657d9e708dafbd253be7d61510943a9536b8ec5
Abhishek Verma <abhishekverm2018@gmail.com>
2023-11-27 16:38:54 +0530

    added the working code for setting the window placement and dimension

--- 1_put_window_to_fixed_location.py
+++ 1_put_window_to_fixed_location.py
@@ -1,1 +1,28 @@
-print("Hello World")
+"""This program set the area for the emulator to run in.
+It is important as our input neurons need pixels as input in a fixed
+number.
+number of pixels = width * height"""
+
+import pygetwindow as gw
+
+# Find the window by its title
+window_title = "MEmu"
+window = gw.getWindowsWithTitle(window_title)
+
+# Check if the window is found
+if window:
+    window = window[0]  # Assuming there's only one window with the given title
+    print(window.size)
+    print(window.topleft)
+
+    # Resize the window
+    new_width = 600
+    new_height = 1035
+    window.resizeTo(new_width, new_height)
+
+    # Move the window to a specific location (replace these coordinates with desired values)
+    new_x = 1300
+    new_y = 0
+    window.moveTo(new_x, new_y)
+else:
+    print(f"Window with title '{window_title}' not found.")
